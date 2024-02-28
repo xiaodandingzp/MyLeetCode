dd12c32bd41b951681c1a0664f0dd71efd5b6107
xiaodandingzp <2440788861@qq.com>
2024-02-28 10:01:15 +0800

    [13] 83 没有重复元素组合的全排列

--- app/src/main/java/com/example/myleetcode/ui/theme/zuhe13/Zuhe13.java
+++ app/src/main/java/com/example/myleetcode/ui/theme/zuhe13/Zuhe13.java
@@ -5,8 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Comparator;
 
+/**
+ * 回溯法：如果一个问题需要若干步骤，并且在每一个步都面临若干选选项，那么可以用回溯法来解决。
+ * 回溯法解决的问题有一个特点，就是这个问题存在多个解，而题目往往需要列出所有的解法
+ */
 public class Zuhe13 {
 
+//    82 由于每个元素都有两种选择，所以时间复杂度是2的n次方
     public List<List<Integer>> combinationSum(List<Integer> array, int target) {
         array.sort(new Comparator<Integer>() {
             @Override
@@ -40,6 +45,35 @@ public class Zuhe13 {
             index++;
         }
         return index;
-
     }
+
+
+
+//    83 没有重复元素组合的全排列  时间复杂度：n!
+    List<List<Integer>> test2(List<Integer> array) {
+        List<List<Integer>> result = new ArrayList<>();
+        test2(array, 0, result);
+        return result;
+    }
+
+    void test2(List<Integer> array, int index, List<List<Integer>> result) {
+        if (index == array.size()) {
+            result.add(new ArrayList<>(array));
+        } else {
+            for (int j = index; j < array.size(); j++) {
+                swap(array, j, index);
+                test2(array, index + 1, result);
+                swap(array, j, index);
+            }
+        }
+    }
+
+    void swap(List<Integer> array, int first, int second) {
+        if (first != second) {
+            int temp = array.get(first);
+            array.set(first, array.get(second));
+            array.set(second, temp);
+        }
+    }
+
 }
