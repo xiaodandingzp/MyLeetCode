b0e1d9d28ac728ad43971028fb1964d64811aab8
xiaodandingzp <2440788861@qq.com>
2024-04-16 19:29:10 +0800

    动态规划 最少分割次数形成的子串都是回文串

--- app/src/main/java/com/example/myleetcode/ui/theme/dymic/Dynamic.java
+++ app/src/main/java/com/example/myleetcode/ui/theme/dymic/Dynamic.java
@@ -17,4 +17,34 @@ public class Dynamic {
         result = Math.min(dp[last][0], Math.min(dp[last][1], dp[last][2]));
         return result;
     }
+
+
+//    回文串 最少分割次数
+    public int minCut(String s) {
+        int length = s.length();
+        if (length <= 1) return length;
+        boolean[][] isH = new boolean[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j + 1 >= i || isH[j + 1][i - 1])) {
+                    isH[j][i] = true;
+                }
+            }
+        }
+
+        int[] dp = new int[length];
+        for (int i = 1; i < length; i++) {
+            if (isH[0][i]) {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i - 1] + 1;
+                for (int j = 0; j < i; j++) {
+                    if (isH[j][i]) {
+                        dp[i] = Math.min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+        return dp[length - 1];
+    }
 }
