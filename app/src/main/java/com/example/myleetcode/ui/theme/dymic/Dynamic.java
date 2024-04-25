74aa9962fb198e497c42382a726ddf5d62d0d528
xiaodandingzp <2440788861@qq.com>
2024-04-25 14:03:45 +0800

    动态规划 字符串s有多少个子串t

--- app/src/main/java/com/example/myleetcode/ui/theme/dymic/Dynamic.java
+++ app/src/main/java/com/example/myleetcode/ui/theme/dymic/Dynamic.java
@@ -47,4 +47,39 @@ public class Dynamic {
         }
         return dp[length - 1];
     }
+
+    public int numDistinct(String s, String t) {
+        if (s.length() < t.length()) return 0;
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        dp[0][0] = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            dp[i + 1][0] = 1;
+            for (int j = 0; j < t.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + dp[i][j + 1];
+                } else {
+                    dp[i + 1][j + 1] = dp[i][j + 1];
+                }
+            }
+        }
+
+        return dp[s.length()][t.length()];
+    }
+
+
+    public int numDistinct1(String s, String t) {
+        if (s.length() < t.length()) return 0;
+        int[] dp = new int[t.length() + 1];
+
+        for (int i = 0; i < s.length(); i++) {
+            dp[0] = 1;
+            for (int j = 0; j < t.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[j + 1] += dp[j];
+                }
+            }
+        }
+        return dp[t.length()];
+    }
 }
