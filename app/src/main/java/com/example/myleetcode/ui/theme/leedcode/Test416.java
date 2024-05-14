fca746f8dc0766334548cdc7faffda904039d4fd
xiaodandingzp <2440788861@qq.com>
2024-05-14 15:22:00 +0800

    动态规划: 背包问题 leedcode 416

--- app/src/main/java/com/example/myleetcode/ui/theme/leedcode/Test416.java
+++ app/src/main/java/com/example/myleetcode/ui/theme/leedcode/Test416.java
@@ -19,4 +19,18 @@ public class Test416 {
         }
         return dp[target] > 0;
     }
+
+    public boolean canPartition1(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = target; j >= nums[i -1]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i -1]];
+            }
+        }
+        return dp[target];
+    }
 }
