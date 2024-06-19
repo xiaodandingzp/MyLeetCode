834b1b9e2d7e628d4b8b8e6c9ed0d9d4e919e9d5
xiaodandingzp <2440788861@qq.com>
2024-06-19 15:53:59 +0800

    滑动窗口大小为3 计算滑动过程中，滑动窗口内的最大值

--- app/src/main/java/com/example/myleetcode/ui/theme/leedcode/TestZp1.java
+++ app/src/main/java/com/example/myleetcode/ui/theme/leedcode/TestZp1.java
@@ -1,6 +1,8 @@
 package com.example.myleetcode.ui.theme.leedcode;
 
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Stack;
 
 // 计算"3 + (4 + (3 -2)) + 5" 的值
@@ -68,5 +70,35 @@ public class TestZp1 {
 
     private boolean isNumber(char c) {
         return c >= '0' && c <= '9';
+    }
+
+
+
+//    滑动窗口大小为3 计算滑动过程中，滑动窗口内的最大值
+    public int[] complute(int[] array) {
+        if (array.length <= 3) {
+            int[] result = new int[1];
+            for (int i = 0; i < array.length; i++) {
+                result[0] = Math.max(array[i], result[0]);
+            }
+            return result;
+        }
+        int[] result = new int[array.length - 2];
+        PriorityQueue<Integer> temp = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int i = 0; i < array.length && i < 3; i++) {
+            temp.add(array[i]);
+        }
+        result[0] = temp.peek();
+        for (int i = 3; i < array.length; i++) {
+            temp.remove(array[i - 3]);
+            temp.add(array[i]);
+            result[i - 2] = temp.peek();
+        }
+        return result;
     }
 }
