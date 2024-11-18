957fc29ddffa2442df9b45291c6d645f602a9446
xiaodandingzp <2440788861@qq.com>
2024-11-18 23:02:24 +0800

    测试代码-Arrays、Collections、list转成array

--- app/src/main/java/com/example/myleetcode/ui/theme/leedcode/Test151.java
+++ app/src/main/java/com/example/myleetcode/ui/theme/leedcode/Test151.java
@@ -10,6 +10,19 @@ public class Test151 {
         s = s.trim();
         List<String> list = Arrays.asList(s.split(" "));
         Collections.reverse(list);
+//        list转成array
+//        Arrays.sort(list.toArray(new String[list.size()]), new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return 0;
+//            }
+//        });
+//        Collections.sort(list, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return 0;
+//            }
+//        });
         return String.join(" ", list);
     }
 }
